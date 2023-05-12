0e844e505fb0c7ae8cc6c40210cec34bd0baecc0
Seraphim97 <savichevserafim@gmail.com>
2023-05-12 20:46:32 +0300

    #Ex.24

--- maven/yml.java
+++ maven/yml.java
@@ -1,27 +1,3 @@
 public class yml {
-    name: Java CI simple pipeline with cron
-    on:
-        push:
-            branches: ['*']
-    pull_request:
-      branches:["main"]
-    workflow_dispatch:
-    schedule:
-    - cron: "30 2 * * *"
 
-
-//    name: Java CI with Maven - manual run and prepare Allure
-//
-//    on:
-//        push:
-//          branches: ['*']
-//        pull_request:
-//          branches:["main"]
-//        workflow_dispatch:
-//            inputs:
-//                testClassName:
-//                  description: 'SimpleTestWithStatuses'
-//                  required: true
-//    -name: Run tests
-//            run: mvn test -Dtest=${{ inputs.SimpleTestWithStatuses}}
 }
