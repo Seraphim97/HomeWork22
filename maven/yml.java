3f2570bedc0deda1ac336cd00a71810e4e41e8b2
Seraphim97 <savichevserafim@gmail.com>
2023-05-12 20:04:08 +0300

    #Ex.24

--- maven/yml.java
+++ maven/yml.java
@@ -8,4 +8,20 @@ public class yml {
     workflow_dispatch:
     schedule:
     - cron: "30 2 * * *"
+
+
+    name: Java CI with Maven - manual run and prepare Allure
+
+    on:
+        push:
+          branches: ['*']
+        pull_request:
+          branches:["main"]
+        workflow_dispatch:
+            inputs:
+                testClassName:
+                  description: 'SimpleTestWithStatuses'
+                  required: true
+    -name: Run tests
+            run: mvn test -Dtest=${{ inputs.SimpleTestWithStatuses }}
 }
